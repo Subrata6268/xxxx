c5a2d4b11faf068a376b623a5817ad47bfa00b72
SUBRATA DAS <sd588576@gmail.com>
2023-03-03 19:22:14 +0530

    Add project files.

---
+++ xxxx.Db/Dboperation/categoryoperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xxxx.Models;
+
+namespace xxxx.Db.Dboperation
+{
+    public class categoryoperation
+    {
+        public int savecategory(Categorymodel model) 
+        {
+            using (var context=new EcomdbEntities()) 
+            {
+                Category cc = new Category() 
+                {
+                Name=model.Name,
+                Description=model.Description,
+                ProductID=model.ProductID
+                };
+                context.Category.Add(cc);
+                context.SaveChanges();
+                return cc.Id;
+            }
+        }
+    }
+}
---
+++ xxxxx/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using xxxx.Db;
+using xxxx.Models;
+using xxxx.Db.Dboperation;
+
+namespace xxxxx.Controllers
+{
+    public class HomeController : Controller
+    {
+        categoryoperation res = new categoryoperation();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Categorymodel model)
+        {
+            if (ModelState.IsValid) 
+            {
+                int id = res.savecategory(model);
+                if (id>0) 
+                {
+                    ModelState.Clear();
+                    ViewBag.msg = "insert successfully";
+                }
+            }
+            return View();
+        }
+    }
+}
